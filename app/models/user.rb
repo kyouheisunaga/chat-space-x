8016765a7442bdf86968433f807e2cdeb824be1c
kyouheisunaga <sunagatheflame@gmail.com>
2020-04-06 09:23:58 +0900

    グループの新規登録、編集機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, uniqueness: true
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
